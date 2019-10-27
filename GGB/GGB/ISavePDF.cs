900b8c0bd7603383f4c85ac10eb993981237ab91
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-27 12:10:26 +0500

    add SavePDF

---
+++ GGB/GGB/ISavePDF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGB
+{
+    public interface ISavePDF
+    {
+
+        void Save(List<string> title, List<string> data);
+
+
+    }
+}
