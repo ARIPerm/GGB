9bfc6fc34535566d0148f3de4d4be0750103e5b7
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-27 16:46:39 +0500

    add title Excel for table

--- GGB/GGB/SaveExcel.cs
+++ GGB/GGB/SaveExcel.cs
@@ -33,11 +33,15 @@ namespace GGB
 
                     int startColumn = 1 ;
 
-                    int row = 1;
+                    int row = 2;
                     int column = 1;
 
-                    //TODO: добавить заголовок, выделить цветом имя столбцов, нарисовать линии таблицы, изменить шрифт
+                    //TODO: выделить цветом имя столбцов, нарисовать линии таблицы, изменить шрифт                 
 
+                    Excel.Range range = (Excel.Range)worksheet.get_Range("B1", "E1").Cells;
+                    range.Merge();
+                    worksheet.Cells[1, 2] = title;
+                    
 
                     for (int i = 0; i < titleColumn.Count; i++)
                     {
@@ -73,6 +77,9 @@ namespace GGB
         public void Close()
         {
             //TODO: закрыть через процессы
+
+            workbook.Close();
+            application.Quit();
         }
     }
 }
