cced1198de76d2fb8d4e3cda27711916c3f57a84
Antony <alogish@yandex.ru>
2019-11-02 20:36:09 +0500

    Создал в модел заглушки но без данных

--- GGB/GGB/Model.cs
+++ GGB/GGB/Model.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace GGB
 {
-    public class Model
+    public class Student
     {
-
-    }
+        string name, surname, patronymic;
+        int id, age, rating, faculty, univer;
+        //public ReadName(name);
+        //public ReadSurname(surname);
+        //public ReadPatronymic(patronymic);
+    };
+    public class Students
+        {
+        public List<Student> students = new List<Student>();
+        };
+    interface IStudents
+        {
+        void GetStudents ();
+        };
 }
